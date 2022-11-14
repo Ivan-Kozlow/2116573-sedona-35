fd0f141b45b4d8c44748c91821f79ddd6533ad09
Ivan <ivan-kozlow04@mail.ru>
2022-11-14 18:22:03 +0300

    lock page, when popup open

--- js/scrypt.js
+++ js/scrypt.js
@@ -4,11 +4,14 @@ const btnOpenPopup = document.querySelector('#btnOpenPopup');
 document.addEventListener('click', function (clickObject) {
   if (!clickObject.target.closest('#popup')) {
     popupWrapper.style.display = "none";
+    document.body.classList.remove("lock");
   }
   if (clickObject.target.closest('#popapCloseBtn')) {
     popupWrapper.style.display = "none";
+    document.body.classList.remove("lock");
   }
   if (clickObject.target.closest('#btnOpenPopup')) {
     popupWrapper.style.display = "flex";
+    document.body.classList.toggle("lock");
   }
 })
